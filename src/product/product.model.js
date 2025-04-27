d27f2e2e922b87a10650020532229ec302cfe9f9
Zodiacal0 <javierherrera5513@gmail.com>
2025-04-27 11:38:59 -0600

    feat(product): se modificó el esquema agregando historial entradas y salidas

--- src/product/product.model.js
+++ src/product/product.model.js
@@ -1,58 +1,54 @@
-    import { Schema , model } from "mongoose"
+import { Schema, model } from "mongoose";
 
-    const productSchema = new Schema({
-        name:{
-            type: String,
-            required: [true, "Name is required"],
-            unique: true
-        },
-        description:{
-            type: String,
-            required: [true, "Description is required"]
-        },
-        price:{
-            type: Number,
-            required: [true, "Price is required"]
-        },
-        stock:{
-            type: Number,
-            required: [true, "Stock is required"]
-        },
-        description:{
-            type: String,
-            required: [true, "Description is required"]
-        },
-        price:{
-            type: Number,
-            required: [true, "Price is required"]
-        },
-        stock:{
-            type: Number,
-            required: [true, "Stock is required"]
-        },
-        brand:{
-            type: String,
-            required: [true, "Brand is required"]
-        },
-        code:{
-            type: Number
-        },
-        popularity:{
-            type: Number,
-            default: 0
-        },
-        status:{
-            type: Boolean,
-            default: true
-        }
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        unique: true
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required"]
+    },
+    stock: {
+        type: Number,
+        required: [true, "Stock is required"]
+    },
+    batch: [{
+        type: Schema.ObjectId,
+        ref: "Batch",
+        default: []
+    }],
+    popularity: {
+        type: Number,
+        default: 0
+    },
+    receipts: [{
+        type: Date,
+        default: []
+    }],
+    issues: [{
+        type: Date,
+        default: []
+    }],
+    history: [{
+        type: Date,
+        default: []
+    }],
+    status: {
+        type: Boolean,
+        default: true
+    }
+});
 
-    });
+productSchema.methods.toJSON = function () {
+    const { __v, status, _id, ...product } = this.toObject();
+    product.uid = _id;
+    return product;
+};
 
-    productSchema.methods.toJSON = function(){
-        const { __v, status, _id, ...product } = this.toObject();
-        product.uid = _id;
-        return product;
-    };
-
-
-    export default model("Product", productSchema);
+export default model("Product", productSchema);
